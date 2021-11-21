4f2a8d5b1bd6a06350dd450bef6008842ba562fc
OliverZott <zott_oliver@web.de>
2021-11-21 19:17:49 +0100

    Initial commit.

---
+++ src/main/java/com/velo/microserviceusermanagement/MicroserviceUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.velo.microserviceusermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceUserManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/velo/microserviceusermanagement/MicroserviceUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velo.microserviceusermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
