1d7dbaee11f878c194221857be3611531e79cebd
Oliver Zott <zott_oliver@web.de>
2021-12-06 13:12:03 +0100

    Spring-Security UserDetailsService implemented;

---
+++ src/main/java/com/velo/microserviceusermanagement/service/UserDetailServiceImpl.java
@@ -0,0 +1,38 @@
+package com.velo.microserviceusermanagement.service;
+
+import com.velo.microserviceusermanagement.model.User;
+import com.velo.microserviceusermanagement.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElse(null);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole().name()));
+
+        return new org.springframework.security.core.userdetails.User(username, user.getPassword(), authorities);
+    }
+
+}
