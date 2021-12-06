3035dbd2036e79b4715dd336f18beee6b6c517fe
Oliver Zott <zott_oliver@web.de>
2021-12-06 21:23:41 +0100

    API endpoints for discovery client instances added;

--- src/main/java/com/velo/microserviceusermanagement/controller/UserController.java
+++ src/main/java/com/velo/microserviceusermanagement/controller/UserController.java
@@ -3,23 +3,46 @@ package com.velo.microserviceusermanagement.controller;
 import com.velo.microserviceusermanagement.model.Role;
 import com.velo.microserviceusermanagement.model.User;
 import com.velo.microserviceusermanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 import java.util.List;
 
-@Controller
+@RestController
 public class UserController {
 
-    private final UserService userService;
+    @Autowired
+    private UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private Environment env;
+
+    @Value("${spring.application.name}")
+    private String serviceId;
+
+    @GetMapping("/service/port")
+    public String getPort() {
+        return "Service port number : " + env.getProperty("local.server.port");
+    }
+
+    @GetMapping("/service/instances")
+    public ResponseEntity<?> getInstances() {
+        return new ResponseEntity<>(discoveryClient.getInstances(serviceId), HttpStatus.OK);
+    }
+
+    @GetMapping("/service/services")
+    public ResponseEntity<?> getServices() {
+        return new ResponseEntity<>(discoveryClient.getServices(), HttpStatus.OK);
     }
 
     @PostMapping("/service/registration")
